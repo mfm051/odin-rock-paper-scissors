13f1bf86a64c1983c93cfa49ea9e840f8b956e53
mfm051 <mateusmatos051@gmail.com>
2023-02-15 19:12:29 -0300

    Add complete game function and return the necessary variables

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-// First: Input player Option and save it in a variable
+// Input player choice and save it in a variable
 let getPlayerChoice = () => prompt('Choose rock, paper or scissors').toLowerCase(); //prompt user option and store it in lower case
 
-// Second: make computer "choose" its option
+// Make computer "choose" its option
 function getComputerChoice() {
     let possibleChoices = ['rock','paper','scissors'];
     let choiceIndex = Math.floor(Math.random()*3); // random int between 0 and 2
@@ -9,38 +9,41 @@ function getComputerChoice() {
     return choice;
 }
 
-// Third: compare it:
-//     if playerOption === computerOption: draw
-    
-//     if playerOption === rock && computerOption === scissors: player wins 
-//                                                 === paper: computer wins
-    
-//     if playerOption === paper && computerOption === rock: player wins
-//                                                 === paper: computer wins
-                                                
-//     if playerOption === scissors && computerOption === paper: player wins
-//                                                    === rock: computer wins
+// In each round the choices are compared
                
 function singleRound() {
-    if (getPlayerChoice() === getComputerChoice()) return 'draw';
-    else if (getPlayerChoice() === 'rock' && getComputerChoice() ==='scissors' 
-        || getPlayerChoice() === 'paper' && getComputerChoice() ==='rock' 
-        || getPlayerChoice() === 'scissors' && getComputerChoice() ==='paper') return 'win';
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) return 'draw';
+    else if (playerSelection === 'rock' && computerSelection ==='scissors' 
+        || playerSelection === 'paper' && computerSelection ==='rock' 
+        || playerSelection === 'scissors' && computerSelection ==='paper') return 'win';
     else return 'lose';
 }
-// Fourth: declare winner: ****ACTUALLY**** make a game with five rounds
-//     if playerPoints > computerPoints: player wins; else: computer wins INCOMPLETE: draw is possible
+
+// Declare winner in a game with five rounds
 
 function game() {
     let playerPoints = 0;
     let computerPoints = 0;
     for (let i=0; i<5; i++) {
-        let result = singleRound()
+        result = singleRound()
         if (result === 'win') {
             playerPoints ++;
+            console.log(`Nice! ${playerSelection} beats ${computerSelection}`)
         }
         else if (result === 'lose') {
             computerPoints ++;
+            console.log(`Ouch! ${playerSelection} loses to ${computerSelection}`)
         }
-    }
+        else {
+            console.log("It's a draw!")
+        }
+    };
+    console.log(
+        `Player points: ${playerPoints}\nComputer points: ${computerPoints}`
+    );
+    if (playerPoints === computerPoints) console.log("It's a draw!");
+    if (playerPoints > computerPoints) console.log("You win! Congratulations!");
+    if (playerPoints < computerPoints) console.log("You lose :(");
 }
