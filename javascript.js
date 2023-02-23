5e0b8ef7d90a7926e98a753a3f60d409d8ae32da
mfm051 <mateusmatos051@gmail.com>
2023-02-23 14:55:52 -0300

    Incorporate human and computer choices in a single function

--- javascript.js
+++ javascript.js
@@ -1,60 +1,64 @@
-function getPlayerChoice (event) {
-    const choice = event.target.id;
-    const resultDiv = document.querySelector(".result.human").children[1];
-    resultDiv.className = `option ${choice}`;
-    return choice;
-}
+function singleRound(event) {
+    
+    function getComputerChoice() {
+        let possibleChoices = ['rock','paper','scissors'];
+        let choiceIndex = Math.floor(Math.random()*3); // random int between 0 and 2
+        let choice = possibleChoices[choiceIndex];
+        return choice;
+    };
 
-function getComputerChoice() {
-    const possibleChoices = ['rock','paper','scissors'];
-    const choiceIndex = Math.floor(Math.random()*3); // random int between 0 and 2
-    const choice = possibleChoices[choiceIndex];
-    const resultDiv = document.querySelector(".result.computer").children[1];
-    resultDiv.className = `option ${choice}`;
-    return choice;
-}
+    function printResult(player) {
+        let resultDiv = document.querySelector(`.result.${player}`).children[1];
+        if (player === "human") resultDiv.className = `option ${humanChoice}`
+        else if (player === "computer") resultDiv.className = `option ${computerChoice}`
+        else return false;
+    }
 
-const options = document.querySelectorAll("#rock, #paper, #scissors");
-options.forEach((option) => {   option.addEventListener("mousedown",getPlayerChoice);
-                                option.addEventListener("mouseup",getComputerChoice);
-                            }
-                );
+    let humanChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    printResult('human');
+    printResult('computer')    
 
-                
-// Old code
-function singleRound() {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    if (playerSelection === computerSelection) return 'draw';
-    else if (playerSelection === 'rock' && computerSelection ==='scissors' 
-        || playerSelection === 'paper' && computerSelection ==='rock' 
-        || playerSelection === 'scissors' && computerSelection ==='paper') return 'win';
+    if (humanChoice === computerChoice) return 'draw';
+    else if (humanChoice === 'rock' && computerChoice ==='scissors' 
+        || humanChoice === 'paper' && computerChoice ==='rock' 
+        || humanChoice === 'scissors' && computerChoice ==='paper') return 'win';
     else return 'lose';
 }
 
+const options = document.querySelectorAll("#rock, #paper, #scissors");
+options.forEach((option) => {   option.addEventListener("mousedown",singleRound);
+                                // option.addEventListener("mouseup",getComputerChoice);
+                            }
+                );
+
+
+// Old code
+
+
 // Declare winner in a game with five rounds
 
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i=0; i<5; i++) {
-        result = singleRound()
-        if (result === 'win') {
-            playerPoints ++;
-            console.log(`Nice! ${playerSelection} beats ${computerSelection}`)
-        }
-        else if (result === 'lose') {
-            computerPoints ++;
-            console.log(`Ouch! ${playerSelection} loses to ${computerSelection}`)
-        }
-        else {
-            console.log("It's a draw!")
-        }
-    };
-    console.log(
-        `Player points: ${playerPoints}\nComputer points: ${computerPoints}`
-    );
-    if (playerPoints === computerPoints) console.log("It's a draw!");
-    if (playerPoints > computerPoints) console.log("You win! Congratulations!");
-    if (playerPoints < computerPoints) console.log("You lose :(");
-}
+// function game() {
+//     let playerPoints = 0;
+//     let computerPoints = 0;
+//     for (let i=0; i<5; i++) {
+//         result = singleRound()
+//         if (result === 'win') {
+//             playerPoints ++;
+//             console.log(`Nice! ${playerSelection} beats ${computerSelection}`)
+//         }
+//         else if (result === 'lose') {
+//             computerPoints ++;
+//             console.log(`Ouch! ${playerSelection} loses to ${computerSelection}`)
+//         }
+//         else {
+//             console.log("It's a draw!")
+//         }
+//     };
+//     console.log(
+//         `Player points: ${playerPoints}\nComputer points: ${computerPoints}`
+//     );
+//     if (playerPoints === computerPoints) console.log("It's a draw!");
+//     if (playerPoints > computerPoints) console.log("You win! Congratulations!");
+//     if (playerPoints < computerPoints) console.log("You lose :(");
+// }
